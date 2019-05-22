a12b85638c9af91acc0ed465dcc7a216e639e959
z14 <z14@z14demac.local>
2019-05-22 08:20:01 +0800

    Initial Commit

---
+++ EducationAdministrators/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EducationAdministrators
+//
+//  Created by z14 on 2019/5/22.
+//  Copyright © 2019年 z14. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
